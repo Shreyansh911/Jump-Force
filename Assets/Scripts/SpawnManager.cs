6a06dbae67c46d1fdbbd2420c6197f2ff3f8a08b
Shreyansh911 <shreyanshvarma01@gmail.com>
2022-03-24 22:55:02 +0530

    Initial commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsticlePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObsticle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObsticle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obsticlePrefab, spawnPos, obsticlePrefab.transform.rotation);
+        }
+           
+    }
+}
